d7605d711be4dc8e68a8ed02555a244c3e28cad6
miralsuthar <miralsuthar2000@gmail.com>
2023-02-10 21:55:16 +0530

    fix: navbar

--- packages/frontend/pages/create.tsx
+++ packages/frontend/pages/create.tsx
@@ -3,7 +3,6 @@ import { Navbar, ImageDroper, Prompt } from "@/components";
 export default function Create() {
   return (
     <div className="h-full w-full">
-      <Navbar />
       <section className="text-white flex pt-20 w-full h-full gap-10">
         <div className="h-4/6 w-4/12">
           <ImageDroper />

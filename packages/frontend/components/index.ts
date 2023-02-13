2e014ae1bec88e938fc251a894de53cbd162bd15
miralsuthar <miralsuthar2000@gmail.com>
2023-02-13 14:31:26 +0530

    feat: add navbar pill animation

--- packages/frontend/components/index.ts
+++ packages/frontend/components/index.ts
@@ -2,3 +2,4 @@ export { Layout } from "./Layout/Layout";
 export { Navbar } from "./Layout/Navbar";
 export { ImageDropper } from "./ImageDropper";
 export { Prompt } from "./Prompt";
+export { CustomConnectButton } from "./ConnectButton";

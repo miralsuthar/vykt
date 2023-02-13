9ba2fa890764b66a7a6eb74601019c4b0e3231a5
miralsuthar <miralsuthar2000@gmail.com>
2023-02-14 00:05:08 +0530

    feat: linking image to address using contract interaction

--- packages/frontend/components/ImageDropper/index.tsx
+++ packages/frontend/components/ImageDropper/index.tsx
@@ -3,7 +3,12 @@ import Cropper from "react-easy-crop";
 import clsx from "clsx";
 import { FiCrop, FiUpload } from "react-icons/fi";
 import { v4 as uuidv4 } from "uuid";
-import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import { utils } from "ethers";
+import {
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
 
 import { storeFile, getImageURI } from "@/utils/web3storageHelpers";
 
@@ -27,18 +32,19 @@ export function ImageDropper() {
     address: "0x4C521043f07be5726b549A5A069BD048f2E333d0",
     abi: VykyABI,
     functionName: "setCurrentImageURI",
-    args: [ipfsUri],
+    args: [
+      ipfsUri,
+      {
+        gasLimit: 1000000,
+        value: utils.parseEther("0.1"),
+      },
+    ],
   });
 
-  const { data, isLoading, isSuccess, write } = useContractWrite(config);
-
-  useEffect(() => {
-    if (ipfsUri) {
-      write?.();
-      setIsImageSaving(false);
-      console.log("its done hoorah");
-    }
-  }, [ipfsUri]);
+  const { data, write, isError, error } = useContractWrite(config);
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
 
   const handleSaveVykt = async () => {
     if (image) {
@@ -53,6 +59,20 @@ export function ImageDropper() {
         getImageURI(cid, imageId)
       );
       setIpfsUri(ImageUri!);
+      setIsImageSaving(false);
+    }
+  };
+
+  const mint = () => {
+    if (ipfsUri) {
+      write?.();
+      setIsImageSaving(isLoading);
+    }
+    if (isError) {
+      console.log(error?.message);
+    }
+    if (isSuccess) {
+      console.log("success");
     }
   };
 
@@ -181,10 +201,20 @@ export function ImageDropper() {
         <button className={buttonClass}>Preview</button>
         <button
           className={`${buttonClass} bg-blue-600`}
-          onClick={handleSaveVykt}
+          onClick={() => {
+            handleSaveVykt();
+          }}
           disabled={isImageSaving}
         >
           {isImageSaving ? "Processing..." : "Save your Vykt"}
+        </button>
+        <button
+          className={`${buttonClass} bg-blue-600`}
+          onClick={() => {
+            mint();
+          }}
+        >
+          Link to address
         </button>
       </div>
     </div>

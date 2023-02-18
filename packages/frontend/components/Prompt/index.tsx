af13cc19da0894ae3e0227faf2394331b4df3f27
Keshav Sharma <skeshav0825@gmail.com>
2023-02-18 16:49:31 +0530

    feat: added infinite-scroll-component but requires a lot of fix

--- packages/frontend/components/Prompt/index.tsx
+++ packages/frontend/components/Prompt/index.tsx
@@ -7,6 +7,9 @@ import clsx from "clsx";
 import { useAccount } from "wagmi";
 import { NftImageCard } from "./NftImageCard";
 import { PromptImageCard } from "./PromptImageCard";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { SearchResponse, Image } from "@/utils/types";
+import axios from "axios";
 
 const WorkSans = Work_Sans({
   weight: "500",
@@ -27,6 +30,8 @@ export function Prompt() {
   const [cursor, setCursor] = useState<number>(0);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [tab, setTab] = useState<"prompt" | "nfts">("prompt");
+  const [promptData, setPromptData] = useState<SearchResponse | null>(null);
+  const [promptImages, setPromptImages] = useState<Image[]>([]);
 
   const menuRef = useRef<HTMLDivElement>(null);
   const hoverRef = useRef<HTMLDivElement>(null);
@@ -35,12 +40,33 @@ export function Prompt() {
 
   const { setImage } = useContext(ImageContext);
 
-  const { data: promptData } = useFetchImages(searchParam, cursor);
   const { data: nftData } = useFetchNfts(address as string);
 
+  function getImages() {
+    try {
+      axios({
+        method: "POST",
+        url: `/api/prompt-search?prompt=${prompt}&cursor=${cursor}`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => {
+        setPromptData(res.data);
+        setPromptImages([...promptImages, ...res.data.images]);
+        console.log("res", res.data.images.length);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   useEffect(() => {
-    console.log("data", nftData);
-  }, [nftData]);
+    getImages();
+  }, [prompt]);
+
+  useEffect(() => {
+    console.log("cursor", cursor);
+  }, [cursor]);
 
   useEffect(() => {
     hoverRef.current!.style.width = `${item1Ref.current?.offsetWidth}px`;
@@ -98,27 +124,40 @@ export function Prompt() {
               }
             }}
           />
-          <div className="grid grid-cols-3 justify-items-center scrollbar scrollbar-w-2 scrollbar-track-[#202738] scrollbar-thumb-rounded-full scrollbar-thumb-white  gap-x-2 h-full gap-y-2 overflow-y-scroll">
-            {promptData &&
-              promptData?.images.map((image) => (
-                <PromptImageCard
-                  isSelected={image.id === selectedImage}
-                  onClick={() => {
-                    fetch(promptData?.baseURL + "/" + image?.id)
-                      .then((res) => res.blob())
-                      .then((promptData) =>
-                        setImage!(
-                          new File([promptData], "image", {
-                            type: promptData.type,
-                          })
-                        )
-                      );
-                    setSelectedImage(image.id);
-                  }}
-                  key={image.id}
-                  src={promptData?.baseURL + "/" + image?.id}
-                />
-              ))}
+          <div className="scrollbar scrollbar-w-2 scrollbar-track-[#202738] scrollbar-thumb-rounded-full scrollbar-thumb-white overflow-y-scroll">
+            <InfiniteScroll
+              dataLength={promptImages ? promptImages.length! : 0} //This is important field to render the next data
+              next={() => {
+                setCursor(promptData?.nextCursor!);
+                getImages();
+                console.log("next");
+              }}
+              hasMore={true}
+              loader={<div></div>}
+            >
+              <div className="grid grid-cols-3 justify-items-center gap-x-2 h-full gap-y-2">
+                {promptImages.length > 0 &&
+                  promptImages.map((image) => (
+                    <PromptImageCard
+                      isSelected={image.id === selectedImage}
+                      onClick={() => {
+                        fetch(promptData?.baseURL + "/" + image?.id)
+                          .then((res) => res.blob())
+                          .then((promptData) =>
+                            setImage!(
+                              new File([promptData], "image", {
+                                type: promptData.type,
+                              })
+                            )
+                          );
+                        setSelectedImage(image.id);
+                      }}
+                      key={image.id}
+                      src={promptData?.baseURL + "/" + image?.id}
+                    />
+                  ))}
+              </div>
+            </InfiniteScroll>
           </div>
         </>
       ) : nftData?.assets.length! > 0 ? (
